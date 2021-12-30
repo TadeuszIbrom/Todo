71bbe47009a648d183ae5a0ada4ce0c6d276a2b3
Tadeusz Ibrom <tadeuszibrom@gmail.com>
2021-12-30 19:14:29 +0100

    First working version

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,41 @@
 {
     const tasks = [
-
         { content: "Wysłać list", done: true, },
         { content: "Wysłać kasę", done: false, },
     ];
 
     const render = () => {
-        let htmlcode = ""
-        for (task of tasks) {
-            htmlcode += `<li>
-            ${task.content}</li>`
-        }
-        document.querySelector(".js-taskList").innerHTML= htmlcode;
+        let htmlcode = "";
+        for (const task of tasks) {
+            htmlcode += `<li ${task.done ? "class=\"taskDone\"" : "class=\"taskUndone\""}>${task.content};
+            `;
+            document.querySelector(".js-tasksList").innerHTML = htmlcode;
+        };
     };
+
+   
+    const addNewTask = (newTaskContent) => {
+        tasks.push({ content: newTaskContent, });
+        render();
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-addTask").value.trim();
+        if (newTaskContent === "") { return; }
+        addNewTask(newTaskContent);
+    }
+
 
     const init = () => {
-        console.log(tasks);
         render();
+      
+        const Form = document.querySelector(".js-toDoForm");
 
-    };
+        Form.addEventListener("submit", onFormSubmit);
 
+    }
     init();
 
 };
-
-
-
