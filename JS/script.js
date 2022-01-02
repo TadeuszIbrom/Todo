2493a1cf5c6fcfc31889aca40b64f6155d7b61d5
Tadeusz Ibrom <tadeuszibrom@gmail.com>
2022-01-02 10:18:43 +0100

    add taskDone button and add bindEvents

--- JS/script.js
+++ JS/script.js
@@ -13,7 +13,35 @@
         tasks.splice(index, 1);
         render()
     }
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+            done: false
+        });
+        render();
+    }
+    const taskDone = (index) => {
+        tasks[index].done = !tasks[index].done;
+        render()
 
+
+
+    }
+    bindEvents=()=>{
+        const taskDoneButtons = document.querySelectorAll(".js-taskDone");
+        taskDoneButtons.forEach
+            ((taskDoneButton, index) => {
+                taskDoneButton.addEventListener
+                    ("click", () => { taskDone(index); });
+            });
+
+            const removeButtons = document.querySelectorAll(".js-removeTask");
+            removeButtons.forEach
+                ((removeButton, index) => {
+                    removeButton.addEventListener
+                        ("click", () => { removeTask(index); });
+                });
+    }
 
 
 
@@ -22,18 +50,11 @@
         let htmlcode = "";
         for (const task of tasks) {
             htmlcode += `<li ${task.done ? "class=\"taskDone\"" : "class=\"taskUndone\""}>                  
-            <button class="js-importantTask">Ważne</button>${task.content}<button class="js-removeTask">Usuń</button>
+            <button class="js-importantTask">Ważne</button>---<button class="js-taskDone">Zrobione</button>${task.content}<button class="js-removeTask">Usuń</button>
             `;
             document.querySelector(".js-tasksList").innerHTML = htmlcode;
 
-            const removeButtons = document.querySelectorAll(".js-removeTask");
-            removeButtons.forEach
-                ((removeButton, index) => {
-                    removeButton.addEventListener
-                        ("click", () => { removeTask(index); });
-                });
-
-
+           bindEvents();
 
         };
     }
@@ -43,13 +64,7 @@
 
 
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-            done: false
-        });
-        render();
-    }
+
 
     const onFormSubmit = (event) => {
         event.preventDefault();
