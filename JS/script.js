74d9a1efd34dcd25c42fa5e18ca71efa0a24fa80
Tadeusz Ibrom <tadeuszibrom@gmail.com>
2022-01-02 19:20:40 +0100

    to taskList added fontAwesome

--- JS/script.js
+++ JS/script.js
@@ -50,7 +50,12 @@
         let htmlcode = "";
         for (const task of tasks) {
             htmlcode += `<li ${task.done ? "class=\"taskDone\"" : "class=\"taskUndone\""}>                  
-            <button class="js-importantTask">Ważne</button>---<button class="js-taskDone">Zrobione</button>${task.content}<button class="js-removeTask">Usuń</button>
+            <button class="button importantTask js-importantTask"><i class="fa fa-exclamation" aria-hidden="true"></i>
+            </button>
+            <button class="button done js-taskDone"><i class="fa fa-check" aria-hidden="true"></i>
+            </button>
+            <span class="taskContent">${task.content}</span>
+            <button  class="button  removeTask js-removeTask"><i class="fas fa-trash"></i></button>
             `;
             
 
