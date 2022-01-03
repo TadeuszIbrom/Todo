5e462df651c5b7c5f69bc08b6f1a3e8fb2013bd3
Tadeusz Ibrom <tadeuszibrom@gmail.com>
2022-01-03 17:07:34 +0100

    Importenttask button issue fix

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,12 @@
     const tasks = [{
         content: "Wysłać list",
         done: true,
+        importent: false
     },
     {
         content: "Wysłać kasę",
         done: false,
+        importent: false
     },
     ];
 
@@ -16,18 +18,28 @@
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
-            done: false
+            done: false,
+            importent: false,
         });
         render();
     }
     const taskDone = (index) => {
         tasks[index].done = !tasks[index].done;
         render()
-
-
+    }
+    const toogleImportentTask = (index) => {
+        console.log(tasks)
+        tasks[index].importent = !tasks[index].importent;
+        
+        if (tasks[index].importent === true) {
+            const importentTask = document.querySelectorAll(".task");
+            importentTask[index].classList.toggle("importanttask");
+        } return;
+render()
 
     }
-    bindEvents=()=>{
+
+    bindEvents = () => {
         const taskDoneButtons = document.querySelectorAll(".js-taskDone");
         taskDoneButtons.forEach
             ((taskDoneButton, index) => {
@@ -35,12 +47,20 @@
                     ("click", () => { taskDone(index); });
             });
 
-            const removeButtons = document.querySelectorAll(".js-removeTask");
-            removeButtons.forEach
-                ((removeButton, index) => {
-                    removeButton.addEventListener
-                        ("click", () => { removeTask(index); });
-                });
+        const removeButtons = document.querySelectorAll(".js-removeTask");
+        removeButtons.forEach
+            ((removeButton, index) => {
+                removeButton.addEventListener
+                    ("click", () => { removeTask(index); });
+            });
+
+
+        const importantTasks = document.querySelectorAll(".importantTask");
+        importantTasks.forEach
+            ((importantTask, index) => {
+                importantTask.addEventListener
+                    ("click", () => { toogleImportentTask(index); });
+            });
     }
 
 
@@ -49,7 +69,7 @@
     const render = () => {
         let htmlcode = "";
         for (const task of tasks) {
-            htmlcode += `<li ${task.done ? "class=\"taskDone\"" : "class=\"taskUndone\""}>                  
+            htmlcode += `<li ${task.done ? "class=\"task taskDone\"" : "class=\"task taskUndone\""}>                  
             <button class="button importantTask js-importantTask"><i class="fa fa-exclamation" aria-hidden="true"></i>
             </button>
             <button class="button done js-taskDone"><i class="fa fa-check" aria-hidden="true"></i>
@@ -57,12 +77,12 @@
             <span class="taskContent">${task.content}</span>
             <button  class="button  removeTask js-removeTask"><i class="fas fa-trash"></i></button>
             `;
-            
+
 
         };
         document.querySelector(".js-tasksList").innerHTML = htmlcode;
 
-           bindEvents();
+        bindEvents();
     }
 
 
@@ -88,6 +108,7 @@
         const Form = document.querySelector(".js-toDoForm");
         Form.addEventListener("submit", onFormSubmit);
         render();
+        bindEvents();
     }
     init();
 
